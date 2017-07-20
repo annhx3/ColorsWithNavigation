eb299c67574b3e80faf707b4d2083c155cb84291
Alyssa Nielsen <alyssanielsen@alyssas-mbp.centurylink.net>
2017-07-19 21:43:04 -0500

    Project base created - Colors UI Table View and background colors that match title.

---
+++ Colors with Navigation/Colors with Navigation/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  Colors with Navigation
+//
+//  Created by Alyssa Nielsen on 7/19/17.
+//  Copyright © 2017 Alyssa Nielsen. All rights reserved.
+//
+
+import UIKit
+
+struct Color{
+    
+    let name: String
+    let uiColor: UIColor
+}
+
